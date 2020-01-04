abe621fb618e87e5f319a24f1f3d4821913b53f7
alaverma <alaverma@uoc.edu>
2020-01-04 20:43:26 +0100

    Clean text

--- socialnet.py
+++ socialnet.py
@@ -6,23 +6,32 @@ Created on Sat Dec 28 16:09:52 2019
 @author: andreslaverdemarin
 """
 
+# Llireries necesraies per la recoleczió de dades
 import tweepy
 import GetOldTweets3 as got
 import networkx as nx
-# Importamos PyMongo
+# Importamos PyMongo per l'emmagatzematge
 from pymongo import MongoClient
 import pymongo
 
+# Llibreries necesaries pel tractament dels tweets
 import operator
 import datetime
-
 import pandas as pd
 import json
 import math
 
+# Llibreries necesaries per trobar les paraules més rellevants
+import re
+from nltk.corpus import stopwords
+from wordcloud import WordCloud, STOPWORDS
+from stop_words import get_stop_words
+import matplotlib.pyplot as plt
+
 
 class tweetnet():
     
+    # Diferents valors generics fets servir durant el programa
     def __init__(self):
         
         # Definim les claus d'acces a l'API
@@ -49,7 +58,8 @@ class tweetnet():
         
         # Definim l'API que és la que farem servir per fer grides
         self.api = tweepy.API(auth)
-        
+    
+    # Busca els hashtags que és fiquin com a parametre
     def searchHashTag(self, hashTags):
         
         listt = []
@@ -58,7 +68,9 @@ class tweetnet():
         for tweet in tweepy.Cursor(self.api.search,q=(hashTags), count=100, tweet_mode= 'extended').items():
             listt.append(tweet)
             self.db.tweets.insert_one(tweet._json)
-        
+    
+    # Crea el graf amb les dades preses a la funció anterior i el guarda amb
+    # un fitxer .graphml
     def createGraph(self):
         
         DG = nx.DiGraph()
@@ -108,39 +120,10 @@ class tweetnet():
                 else:
                     DG.add_node(A)
                     
-        print(len(DG.nodes), DG.size())
-        
         nx.write_graphml(DG, "./IndependenceTweet.graphml")
         
-        indegree ={} 
-        for node in list(DG.nodes):
-            indegree[node] = DG.in_degree(node, weight='weight')
 
-        degree = sorted(indegree.items(), key=operator.itemgetter(1), reverse=True)
-
-        print(degree[:20])
-        
-    def delete(self):
-        lis = []
-        
-        for tweet in self.db.tweets.find():
-            a = tweet['created_at'].split(" ")
-            datet = a[1]+" "+a[2]+" "+a[-1]
-            date_time_obj = datetime.datetime.strptime(datet, '%b %d %Y')
-            
-            date_time_check = datetime.datetime.strptime("Dec 26 2019", '%b %d %Y')
-            
-            
-            
-            if tweet["id"] == 1211245992030474240:
-                print(tweet)
-                print("\n")
-                lis.append(tweet)
-                
-        print(len(lis))
-        for i in lis[0]:
-            print(i)
-        
+    # Neteja la BD eliminant els tweets repetits       
     def cleanDB(self):
         
        
@@ -155,20 +138,113 @@ class tweetnet():
         
         # Save in mongodb document 
         self.db3.tweets_clean_final.insert_many(df.to_dict(orient='records'))
+    
+    # Selecciona els id per comunitat a la que pertanyen i els guarda en un
+    # diccionari
+    def tweetinfoCommunity(self):
         
+        df = pd.read_pickle("df_tweets.pkl", compression='zip')
         
+        for i in df.columns:
+            print(i)
+        
+        #df["full_text"]
+        
+        df_gephi = pd.read_csv("independence_graf_5_core.csv")
+        
+        modularity_dict = {}
+        for mod in range(0,8):
+            g = df_gephi[df_gephi["modularity_class"] == mod]["Id"].tolist()
+            modularity_dict[mod] = g
+            
+        #for i in modularity_dict:
+        text_dict = {}
+        for mod in modularity_dict:
+            text = []
+            for user_mod in modularity_dict[mod]:
+                b = 0
+                for user in df["user"]:
+                    if user['screen_name'] == user_mod:
+                        text.append(df.iloc[b]["full_text"])
+                    b = b + 1
+                    
+            text_dict[mod] = " ".join(text)
+        
+        return text_dict
+    
+    # Busca i neteja el text de cadascun dels usuaris de tweeter, per comunitats
+    def cleanText(self, dictiory):
+        # This part of code clean text obtained before
+        for group in dictiory.keys():
+    
+            # Delete all mentions
+            final = re.sub(r'@[\w-]+', '', dictiory[group])
+
+            # Delete all hashtags
+            final = re.sub(r'#[\w-]+', '', final)
+
+            # Delete all hashtags
+            final = re.sub(r'RT', '', final)
+
+            # Delete all hashtags
+            final = re.sub(r'&[\w-]+', '', final)
+
+            # Delete all emails
+            final = re.sub(r'[https://]+/[\w\.\/\w-]+', '', final)
+
+            # Delete all puntuation marks except '
+            result = re.sub(r"[^\w\d\s]",'',final)
+
+            # Delete more thant one space 
+            result = re.sub(r"\s+"," ", result)
+    
+            # Delete one letter, nomber or simbol that stay alone in a text
+            result = re.sub(r'(?:^| )\w(?:$| )', ' ', result).strip()
+
+            dictiory[group] = result.lower()
+    
+            # Delete one letter, nomber or simbol that stay alone in a text
+            result = re.sub(r'(?:^| )\w(?:$| )', ' ', result).strip()
+        
+        oder_split_words = ["que", "del", "lo"]
+        # This part of the code count frequence and delete stopwords    
+        for group in dictiory.keys():
+            cleantokens = dictiory[group].split()[:]
+            for token in dictiory[group].split():
+                if token in get_stop_words('catalan') or token in stopwords.words("spanish") or token in stopwords.words("english"):
+                    cleantokens.remove(token)
+            dictiory[group] = " ".join(cleantokens)
+            
+        # Visualitzem el resultat
+        plt.figure(num=None, figsize=(22, 20))
+
+        for group in dictiory.keys():
+           wordcloud = WordCloud(max_font_size=50, max_words=50, 
+                                 background_color="white").generate(dictiory[group])
+    
+           plt.figure(num=None, figsize=(22, 20))
+           #plt.set_title("Group: {}".format(group))
+           plt.imshow(wordcloud, interpolation='bilinear')
+           plt.axis("off")
+           plt.savefig('Text_group_{}.png'.format(group))
+        
+# Execució de codi segons el que necesitem        
 def main():
     
     tw = tweetnet()
     
     hashTags = "#SpainIsAFascistState OR #SentenciaProces OR #Independencia OR #CataluñaEsEspaña OR #recuperemelseny OR #EstamosporTI OR #LlibertatPresosPoliticsiexiliats OR #Llibertatpresospolitics"
     
-    #tw.searchHashTag(hashTags)
+    
+    tw.searchHashTag(hashTags)
     
     tw.cleanDB()
     
     tw.createGraph()
-    #tw.delete()
+    
+    comunity_text = tw.tweetinfoCommunity()
+    
+    tw.cleanText(comunity_text)
     
 if __name__ == "__main__":
     main()
